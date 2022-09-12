1c53e0df62e19b4923d92615cafe48e4e142bd40
Ana Luiza Pardini <aninhapardini.work@gmail.com>
2022-09-11 22:58:57 -0300

    second

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import os
 import discord
 from discord.ext import commands
 
 # Comandos
 
+WELCOME_CHANNEL_ID=int(os.environ['WELCOME_CHANNEL_ID'])
+DISCORD_TOKEN=os.environ['DISCORD_TOKEN']
+BODY_BUILDER_ID=os.environ['BODY_BUILDER_ID']
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -14,19 +18,19 @@ class MyClient(discord.Client):
         if message.content == '!sbt':
             await message.channel.send(f'{message.author.name} quer jogar aquela nostalgia que são os games online e gratis?\n Vem dar play em nosso site: https://gg.sbtgames.com.br/')
         elif message.content == '!bodybuilder':
-            await message.channel.send(f'{message.author.name} @Gamer30mais BIRLLLLL HORA DO SHOW!!! :muscle: :sunglasses:')
+            await message.channel.send(f'{message.author.name} <@{BODY_BUILDER_ID}> BIRLLLLL HORA DO SHOW!!! :muscle: :sunglasses:')
 
 # Bem-Vindo
     async def on_member_join(self, member):
         guild = member.guild
-        canal = discord.utils.get(guild.channels, id=997312257035862057)
+        canal = discord.utils.get(guild.channels, id=WELCOME_CHANNEL_ID)
         embed = discord.Embed(
             title='Seja Bem-Vindo(a)!',
             color=0x0000CD,
             description=f'🎊 | {member.mention} Venha curtir com a galera e conversar com seus Talentos favoritos!\n\n 👮‍♂️ | **Não esquece de ir em:**\n <#996965083328548945> para evitar punições!\n\n 💎 | **Veio pela live?** Então já confere aqui em baixo um pouco sobre nossos <#997314245085642782>!'
         )
 
-        embed.set_author(name=member.name, icon_url=member.avatar_url)
+        embed.set_author(name=member.name, icon_url=member.display_avatar)
 
         embed.set_image(
             url='https://3.bp.blogspot.com/-vzcwGr1RDok/Wu8ID2cKtkI/AAAAAAAAEug/TOraNHUnYkEzJpCpJhLK2MN5iijArB7jwCLcBGAs/s1600/hdfPQx.gif')
@@ -41,4 +45,4 @@ intents.members = True
 
 
 client = MyClient(intents=intents)
-client.run('')
+client.run(DISCORD_TOKEN)
